91058095660b69ac90e0327fdeb141e3f5702ca1
Yusuf-96 <yusufummar649@gmail.com>
2021-08-26 19:50:54 +0300

    deploy changes

--- surveyapp/settings.py
+++ surveyapp/settings.py
@@ -142,6 +142,7 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 import dj_database_url 
