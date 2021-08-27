b1da84912e786e4e051ab13bddc2f383054c730e
Yusuf-96 <yusufummar649@gmail.com>
2021-08-27 12:06:19 +0300

    Deploy commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User, auth
+from django.contrib import auth
 
 # Create your views here.
 
@@ -8,7 +9,7 @@ def login(request):
     if request.method =='POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request, username=username, password = password)
+        user = auth.authenticate(username=username, password = password)
 
         if user is not None:
             auth.login(request, user)
